9f479a6a793646d9332485b2ac87e1238384a210
Emily Oppenheimer <opemily@gmail.com>
2016-09-17 22:57:52 -0400

    complete homework 3

---
+++ week_2/homework_a/problem-3/app/controllers/homework_controller.rb
@@ -0,0 +1,9 @@
+class HomeworkController < ApplicationController
+
+  def index
+  end
+
+  def kitties
+  end
+  
+end # Home Controller
--- week_2/homework_a/problem-3/config/routes.rb
+++ week_2/homework_a/problem-3/config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+    root 'homework#index'
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+  get 'kitties' => 'homework#kitties'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
